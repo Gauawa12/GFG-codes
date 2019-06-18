db643f40eb3f505f738e73cbe530e676fc1dfc56
Gauawa12 <42520099+Gauawa12@users.noreply.github.com>
2019-06-18 16:46:17 +0530

    Heap

---
+++ Kth_largest_element.cpp
@@ -0,0 +1,20 @@
+int KthLargest(int a[],int n,int k){
+    
+    priority_queue<int,vector<int>,greater<int>>pq;
+    
+    for(int i=0;i<k;i++)
+    {
+        pq.push(a[i]);
+    }
+    for(int i=k;i<n;i++)
+    {
+        if(a[i]>pq.top())
+        {
+            pq.pop();
+            pq.push(a[i]);
+        }
+    }
+    
+    
+    
+    return pq.top();
