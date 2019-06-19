8868e23fc310089271e387c2f0eb265b24e85147
Gauawa12 <42520099+Gauawa12@users.noreply.github.com>
2019-06-19 10:45:24 +0530

    no sort STL, effective min heap use

---
+++ Nearly_sorted.cpp
@@ -0,0 +1,29 @@
+void nearlySorted(int arr[], int n, int k){
+    
+    priority_queue<int,vector<int>,greater<int> >pq;
+    for(int i=0;i<=k;i++)
+    {
+        pq.push(arr[i]);
+    }
+    
+    vector<int>ans;
+    
+    for(int i=k+1;i<n;i++)
+    {
+        ans.push_back(pq.top());
+        pq.pop();
+        pq.push(arr[i]);
+    }
+    
+    while(!pq.empty())
+    {
+        ans.push_back(pq.top());
+        pq.pop();
+    }
+    for(auto x:ans)
+    {
+        cout<<x<<" ";
+    }
+    cout<<endl;
+    
+}
